da9055aed60bfeb125902c5ce95db5a294b02ad7
cndivecat <543571480@qq.com>
2024-05-29 22:10:12 +0800

    新增member会员模块

--- member/src/main/java/com/jiawa/train/member/controller/TestController.java
+++ member/src/main/java/com/jiawa/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.jiawa.train;
+package com.jiawa.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "hello world1";
+        return "hello world!!!";
     }
 }
