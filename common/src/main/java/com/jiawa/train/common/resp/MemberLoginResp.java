e77499c78f7c9b5568b0539b6d2b994fdbdebad1
cndivecat <543571480@qq.com>
2024-06-04 20:55:04 +0800

    使用线程本地变量存储会员信息

---
+++ common/src/main/java/com/jiawa/train/common/resp/MemberLoginResp.java
@@ -0,0 +1,40 @@
+package com.jiawa.train.common.resp;
+
+public class MemberLoginResp {
+    private Long id;
+    private String token;
+    private String mobile;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberLoginResp{" +
+                "id=" + id +
+                ", token='" + token + '\'' +
+                ", mobile='" + mobile + '\'' +
+                '}';
+    }
+}
--- member/src/main/java/com/jiawa/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/jiawa/train/member/req/PassengerSaveReq.java
@@ -1,13 +1,12 @@
 package com.jiawa.train.member.req;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
 public class PassengerSaveReq {
     private Long id;
-    @NotNull(message = "【会员ID】不能为空")
+//    @NotNull(message = "【会员ID】不能为空")
     private Long memberId;
     @NotBlank(message = "【姓名】不能为空")
     private String name;
