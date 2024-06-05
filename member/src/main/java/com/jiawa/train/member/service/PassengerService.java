482d80607190ee74c3fa9b65f9a14609c7d7bfaf
cndivecat <543571480@qq.com>
2024-06-05 10:31:00 +0800

    实现sql分页功能

--- member/src/main/java/com/jiawa/train/member/service/PassengerService.java
+++ member/src/main/java/com/jiawa/train/member/service/PassengerService.java
@@ -3,6 +3,7 @@ package com.jiawa.train.member.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjUtil;
+import com.github.pagehelper.PageHelper;
 import com.jiawa.train.common.context.LoginMemberContext;
 import com.jiawa.train.common.util.SnowUtil;
 import com.jiawa.train.member.domain.Passenger;
@@ -37,6 +38,7 @@ public class PassengerService {
         if (ObjUtil.isNotNull(req.getMemberId())){
             criteria.andMemberIdEqualTo(req.getMemberId());
         }
+        PageHelper.startPage(2,2);
         List<Passenger> passengers = passengerMapper.selectByExample(passengerExample);
         return BeanUtil.copyToList(passengers, PassengerQueryResp.class);
     }
