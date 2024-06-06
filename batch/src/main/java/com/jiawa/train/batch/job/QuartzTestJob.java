e476b8ddae2d519327109e882fcbec1ed3301c25
cndivecat <543571480@qq.com>
2024-06-06 15:46:44 +0800

    禁止quartz定时任务并发执行

--- batch/src/main/java/com/jiawa/train/batch/job/QuartzTestJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/QuartzTestJob.java
@@ -1,13 +1,20 @@
 package com.jiawa.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-
+@DisallowConcurrentExecution
 public class QuartzTestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("Quartz job");
+        System.out.println("Quartz job开始");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("Quartz job结束");
     }
 }
--- batch/src/main/java/com/jiawa/train/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/SpringBootTestJob.java
@@ -1,14 +1,14 @@
-package com.jiawa.train.batch.job;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
-//springboot自带的定时任务只适合单体应用，不适合集群（可增加分布式锁）
-//无法实时修改状态与策略，必须修改代码
-@Component
-@EnableScheduling
-public class SpringBootTestJob {
-//    @Scheduled(cron = "0/5 * * * * ?")
-    public void test(){
-        System.out.println("Test Job");
-    }
-}
+//package com.jiawa.train.batch.job;
+//
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.stereotype.Component;
+////springboot自带的定时任务只适合单体应用，不适合集群（可增加分布式锁）
+////无法实时修改状态与策略，必须修改代码
+//@Component
+//@EnableScheduling
+//public class SpringBootTestJob {
+////    @Scheduled(cron = "0/5 * * * * ?")
+//    public void test(){
+//        System.out.println("Test Job");
+//    }
+//}
