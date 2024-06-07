d6867093ee076fdd9184454048cb35ec9f7e5423
cndivecat <543571480@qq.com>
2024-06-07 21:15:21 +0800

    为余票查询添加条件

--- business/src/main/java/com/jiawa/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/com/jiawa/train/business/req/DailyTrainTicketQueryReq.java
@@ -1,12 +1,58 @@
 package com.jiawa.train.business.req;
 
 import com.jiawa.train.common.req.PageReq;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public class DailyTrainTicketQueryReq extends PageReq {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    private String trainCode;
+
+    private String start;
+    private String end;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
 
     @Override
     public String toString() {
         return "DailyTrainTicketQueryReq{" +
+                "date=" + date +
+                ", trainCode='" + trainCode + '\'' +
+                ", start='" + start + '\'' +
+                ", end='" + end + '\'' +
                 "} " + super.toString();
     }
 }
