2ffd75f54eb1b492acda4c6e513ed90a66acbb8d
cndivecat <543571480@qq.com>
2024-06-07 16:45:28 +0800

    添加车座余票表，生成三层代码

---
+++ business/src/main/java/com/jiawa/train/business/req/DailyTrainTicketQueryReq.java
@@ -0,0 +1,12 @@
+package com.jiawa.train.business.req;
+
+import com.jiawa.train.common.req.PageReq;
+
+public class DailyTrainTicketQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainTicketQueryReq{" +
+                "} " + super.toString();
+    }
+}
